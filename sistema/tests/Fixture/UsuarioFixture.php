d47681cddf0db833c9b7c6d1679a30f68ca71f6b
luizaribeir0 <luiza@oneweb.com.br>
2024-09-11 21:45:06 -0300

    Teste

---
+++ sistema/tests/Fixture/UsuarioFixture.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * UsuarioFixture
+ */
+class UsuarioFixture extends TestFixture
+{
+    public string $table = 'usuario';
+
+    /**
+     * Init method
+     *
+     * @return void
+     */
+    public function init(): void
+    {
+        $this->records = [
+            [
+                'id' => 1,
+                'email' => 'teste@gmail.com',
+                'senha' => 'Lorem ipsum dolor sit amet',
+            ],
+        ];
+        parent::init();
+    }
+}
